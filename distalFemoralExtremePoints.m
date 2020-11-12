a8d526ada9324fff41817c2e9b6a58fad7de6104
MCMF <>
2020-11-12 01:09:10 +0100

    standardization

--- distalFemoralExtremePoints.m
+++ distalFemoralExtremePoints.m
@@ -34,8 +34,8 @@ function EP = distalFemoralExtremePoints(distalFemurUSP, side, PFEA, varargin)
 %     Articular Geometry Basedon Three-Dimensional Surface Data
 % 
 % TODO:
-%   - Complete documentation
-%   - Maybe add input of landmarks for additonal sanity checks
+%   - Add input of the possible location of the landmarks for additonal 
+%     sanity checks
 % 
 % AUTHOR: Maximilian C. M. Fischer
 % 	mediTEC - Chair of Medical Engineering, RWTH Aachen University
@@ -69,11 +69,11 @@ sigma = 4;
 if PFEA(6)<0; PFEA(4:6) = -PFEA(4:6); end
 % Intersection points between the mesh an the posterior foci elliptical axis
 [PFEA_IS_Pts, PFEA_Pos] = intersectLineMesh3d(PFEA, distalFemurUSP.vertices, distalFemurUSP.faces);
-% Sort inters. points along the PFEA
+% Sort intersection points along the PFEA
 [PFEA_Pos, sortIdx] = sort(PFEA_Pos);
 PFEA_IS_Pts = PFEA_IS_Pts(sortIdx,:);
 
-% Should always be four inters. points
+% Should always be four intersection points
 PFEA_error = 'PFEA intersection points with the distal femur mesh ~= 4.';
 if size(PFEA_IS_Pts,1) > 4
     % Otherwise try to merge close points
@@ -110,14 +110,14 @@ end
 switch side
     case 'R'
         % RIGHT knee: medial - neg. Z values (NZ), lateral - pos. Z values (PZ)
-        SC(1).Zone = 'NZ';
-        SC(2).Zone = 'MZ';
-        SC(3).Zone = 'PZ';
+        SC(1).Zone = 'NZ'; NZ = 1;
+        SC(2).Zone = 'MZ'; MZ = 2;
+        SC(3).Zone = 'PZ'; PZ = 3;
     case 'L'
         %  LEFT knee: medial - pos. Z values (PZ), lateral - neg. Z values (NZ)
-        SC(1).Zone = 'PZ';
-        SC(2).Zone = 'MZ';
-        SC(3).Zone = 'NZ';
+        SC(1).Zone = 'PZ'; PZ = 1;
+        SC(2).Zone = 'MZ'; MZ = 2;
+        SC(3).Zone = 'NZ'; NZ = 3;
 end
 
 % Start and end point of the zones, rounded to an integer value in Z direction
@@ -142,15 +142,15 @@ LS = size(SC,2);
 ZoneColors = [.3 .3 1; 0 0 1; .6 .6 1];
 
 %% Sagittal Cuts (SC)
-ZVector = [0, 0, 1];
+zVector = [0, 0, 1];
 
 Contours = cell(1,LS);
 for s=1:LS
     SC(s).Color = ZoneColors(s,:);
     % Create cutting plane origins
-    SC(s).PlaneOrigins = repmat(SC(s).Origin, SC(s).NoC, 1) + [zeros(SC(s).NoC,2), (0:SC(s).NoC-1)'];
+    SC(s).PlaneOrigins = repmat(SC(s).Origin, SC(s).NoC,1) + [zeros(SC(s).NoC,2), (0:SC(s).NoC-1)'];
     % Create SC(s).NoC Saggital Contour Profiles (SC(s).P)
-    Contours{s} = IntersectMeshPlaneParfor(distalFemurUSP, SC(s).PlaneOrigins, ZVector);
+    Contours{s} = IntersectMeshPlaneParfor(distalFemurUSP, SC(s).PlaneOrigins, zVector);
     for c=1:SC(s).NoC
         % If there is more than one closed contour after the cut, use the longest one
         [~, IobC] = max(cellfun(@length, Contours{s}{c}));
@@ -182,13 +182,13 @@ for s=1:LS
         % Get the anterior and posterior extreme points of the articulating surface
         switch SC(s).Zone
             case 'NZ'
-                [SC(s).P(c).A, SC(s).P(c).B, SC(s).P(c).H] = ...
+                [SC(s).P(c).P, SC(s).P(c).A, SC(s).P(c).H] = ...
                     sagittalExPts_MedCond(Contour, sigmaStart, sigmaDelta, sigma, debugVisu);
             case 'MZ'
-                [SC(s).P(c).A, SC(s).P(c).B, SC(s).P(c).H] = ...
+                [SC(s).P(c).P, SC(s).P(c).A, SC(s).P(c).H] = ...
                     sagittalExPts_IntCond(Contour, sigmaStart, sigmaDelta, sigma, debugVisu);
             case 'PZ'
-                [SC(s).P(c).A, SC(s).P(c).B, SC(s).P(c).H] = ...
+                [SC(s).P(c).P, SC(s).P(c).A, SC(s).P(c).H] = ...
                     sagittalExPts_LatCond(Contour, sigmaStart, sigmaDelta, sigma, debugVisu);
         end
     end
@@ -196,16 +196,15 @@ end
 
 
 %% Find global extreme points of the sagittal contours (SCs)
-NZ = strcmp({SC(:).Zone}, 'NZ');
-MZ = strcmp({SC(:).Zone}, 'MZ');
-PZ = strcmp({SC(:).Zone}, 'PZ');
 
-% Exclude border of the zones
+% Exclude borders of the zones
+% Intercondylar notch
 if SC(MZ).NoC > 2*BORD_INT
     SC(MZ).ExRange = BORD_INT:SC(MZ).NoC-BORD_INT;
 else
     SC(MZ).ExRange = 1:SC(MZ).NoC;
 end
+% Medial and lateral condyle
 switch side
     case 'R'
         SC(NZ).ExRange = 1:SC(NZ).NoC-BORD_INT;
@@ -216,19 +215,19 @@ switch side
 end
 
 %% Intercondylar Notch (ICN)
-% Take start points (A) of zone MZ specified by ExRange
-A_MZ = nan(SC(MZ).NoC,3);
+% Take posterior points (P) of zone MZ specified by ExRange
+P_MZ = nan(SC(MZ).NoC,3);
 for c = SC(MZ).ExRange
-    A_MZ(c,:) = SC(MZ).P(c).xyz(SC(MZ).P(c).A,:);
+    P_MZ(c,:) = SC(MZ).P(c).xyz(SC(MZ).P(c).P,:);
 end
-A_MZ = A_MZ(~any(isoutlier(A_MZ),2),:);
+P_MZ = P_MZ(~any(isoutlier(P_MZ),2),:);
 % Median of the points A
-A_MZ_median = median(A_MZ,'omitnan');
+P_MZ_median = median(P_MZ,'omitnan');
 
 % Clip region of the ICN
 ICNmesh = cutMeshByPlane(distalFemurUSP, [MZS, 0 1 0, 0 0 1]);
 ANTERIOR_CUT = 5; %[mm]
-ICNmesh = cutMeshByPlane(ICNmesh, [A_MZ_median(1)+ANTERIOR_CUT A_MZ_median(2:3), 0 1 0, 0 0 -1]);
+ICNmesh = cutMeshByPlane(ICNmesh, [P_MZ_median(1)+ANTERIOR_CUT P_MZ_median(2:3), 0 1 0, 0 0 -1]);
 ICNmesh = cutMeshByPlane(ICNmesh, [MZS, 1 0 0, 0  1 0]);
 ICNmesh = cutMeshByPlane(ICNmesh, [MZE, 1 0 0, 0 -1 0]);
 ICNmesh = cutMeshByPlane(ICNmesh, [MZE, 1 0 0, 0 0 1]);
@@ -246,11 +245,15 @@ ICNcontour(any(isnan(ICNcontour),2),:) = [];
 if size(ICNcontour,1) >= 3
     ICNcontour = angleSort3d(ICNcontour, [0 0 0]);
 end
+% Sort contour along the z axis
 [~, ICNcontourSortIdx] = sort(ICNcontour(:,3));
 ICNcontour = ICNcontour(ICNcontourSortIdx,:);
+% Remove duplicate contour points
 [~, ICNcontourUniqueIdx] = uniquetol(ICNcontour(:,3), 3e-2);
 ICNcontour = ICNcontour(ICNcontourUniqueIdx,:);
+% Remove outliers of the contour
 ICNcontour(isoutlier(ICNcontour(:,2),'movmedian',5),:) = nan;
+% Use the part of the contour with the most points
 ICNcontour = splitPolygons(ICNcontour);
 [~, ICNcontourMax] = max(cellfun(@(x) size(x,1),ICNcontour));
 ICNcontour = ICNcontour{ICNcontourMax};
@@ -259,42 +262,42 @@ ICNcontour = ICNcontour{ICNcontourMax};
 [~, ICNconYmaxIdx] = max(ICNcontour(:,2));
 
 % Construct the final ICN point
-ICNpoint = [A_MZ_median(1) ICNcontour(ICNconYmaxIdx,2:3)];
-[~, ICN_Idx] = pdist2(A_MZ,ICNpoint,'euclidean','Smallest',1);
-[~, ICN_Idx] = pdist2(distalFemurUSP.vertices, A_MZ(ICN_Idx,:),'euclidean','Smallest',1);
+ICNpoint = [P_MZ_median(1) ICNcontour(ICNconYmaxIdx,2:3)];
+[~, ICN_Idx] = pdist2(P_MZ,ICNpoint,'euclidean','Smallest',1);
+[~, ICN_Idx] = pdist2(distalFemurUSP.vertices, P_MZ(ICN_Idx,:),'euclidean','Smallest',1);
 EP.Intercondylar = distalFemurUSP.vertices(ICN_Idx,:);
 
-%% Take start points (A) of zone NZ specified by ExRange
-ProximalPosterior_NZ = nan(SC(NZ).NoC,3);
+%% Take posterior points (P) of zone NZ specified by ExRange
+P_NZ = nan(SC(NZ).NoC,3);
 for c = SC(NZ).ExRange
-    ProximalPosterior_NZ(c,:) = SC(NZ).P(c).xyz(SC(NZ).P(c).A,:);
+    P_NZ(c,:) = SC(NZ).P(c).xyz(SC(NZ).P(c).P,:);
 end
-ProximalPosterior_NZ(any(isnan(ProximalPosterior_NZ),2),:)=[];
+P_NZ(any(isnan(P_NZ),2),:)=[];
 % Median of the start points
-[~, PPNZ_Idx] = pdist2(distalFemurUSP.vertices,median(ProximalPosterior_NZ),'euclidean','Smallest',1);
-EP.Medial = distalFemurUSP.vertices(PPNZ_Idx,:);
+[~, MPPC_Idx] = pdist2(distalFemurUSP.vertices,median(P_NZ),'euclidean','Smallest',1);
+EP.Medial = distalFemurUSP.vertices(MPPC_Idx,:);
 
-%% Take start points (A) of zone PZ specified by ExRange
-ProximalPosterior_PZ = nan(SC(PZ).NoC,3);
+%% Take posterior points (P) of zone PZ specified by ExRange
+P_PZ = nan(SC(PZ).NoC,3);
 for c = SC(PZ).ExRange
-    ProximalPosterior_PZ(c,:) = SC(PZ).P(c).xyz(SC(PZ).P(c).A,:);
+    P_PZ(c,:) = SC(PZ).P(c).xyz(SC(PZ).P(c).P,:);
 end
-ProximalPosterior_PZ(any(isnan(ProximalPosterior_PZ),2),:)=[];
+P_PZ(any(isnan(P_PZ),2),:)=[];
 % Median of the start points
-[~, PPPZ_Idx] = pdist2(distalFemurUSP.vertices,median(ProximalPosterior_PZ),'euclidean','Smallest',1);
-EP.Lateral = distalFemurUSP.vertices(PPPZ_Idx,:);
+[~, LPPC_Idx] = pdist2(distalFemurUSP.vertices,median(P_PZ),'euclidean','Smallest',1);
+EP.Lateral = distalFemurUSP.vertices(LPPC_Idx,:);
 
-%% All end points (B) of the zones NZ, MZ, PZ
+%% All end points (A) of the zones NZ, MZ, PZ
 % ProximalAnterior = nan(sum([SC.NoC]),3);
 % countIdx = 1;
 % for s=1:LS
 %     for c = 1:SC(s).NoC
-%         ProximalAnterior(countIdx,:) = SC(s).P(c).xyz(SC(s).P(c).B,:);
+%         ProximalAnterior(countIdx,:) = SC(s).P(c).xyz(SC(s).P(c).A,:);
 %         countIdx = countIdx+1;
 %     end; clear c
 % end
 % ProximalAnterior(any(isoutlier(ProximalAnterior,'movmedian',10),2),:) = nan;
-% % End point RC-MZ: Most proximal point of all end points (B) of the zones NZ, MZ, PZ
+% % End point RC-MZ: Most proximal point of all end points (A) of the zones NZ, MZ, PZ
 % [~, I_ProximalAnterior_Ymax] = max(ProximalAnterior(:,2));
 % EP.Anterior = ProximalAnterior(I_ProximalAnterior_Ymax,:);
 
@@ -309,21 +312,21 @@ if visu
         for c=1:SC(s).NoC
             % Plot contour-part in 3D
             CP3D = SC(s).P(c).xyz;
-            EPA = SC(s).P(c).A; EPB = SC(s).P(c).B;
+            EPA = SC(s).P(c).P; EPB = SC(s).P(c).A;
             plot3(axH, CP3D(EPA:EPB,1),CP3D(EPA:EPB,2),CP3D(EPA:EPB,3),...
                 'Color', SC(s).Color,'Linewidth',1,'LineStyle','--');
         end
         for c=SC(s).ExRange
             % Plot contour-part in 3D
             CP3D = SC(s).P(c).xyz;
-            EPA = SC(s).P(c).A; EPB = SC(s).P(c).B;
+            EPA = SC(s).P(c).P; EPB = SC(s).P(c).A;
             plot3(axH, CP3D(EPA:EPB,1),CP3D(EPA:EPB,2),CP3D(EPA:EPB,3),...
                 'Color', SC(s).Color,'Linewidth',2);
         end
     end
     
-    drawPolyline3d(axH, A_MZ,'Color','b','LineWidth',1,'LineStyle','--')
-    drawPolyline3d(axH, A_MZ,'Color','b','LineWidth',3)
+    drawPolyline3d(axH, P_MZ,'Color','b','LineWidth',1,'LineStyle','--')
+    drawPolyline3d(axH, P_MZ,'Color','b','LineWidth',3)
     
     pointProps.MarkerFaceColor = 'k';
     pointProps.MarkerEdgeColor = 'k';
@@ -350,17 +353,6 @@ if visu
     patch(axH, ICNmesh, patchProps)
     
     anatomicalViewButtons(axH, 'ASR')
-    
-    % % For publication
-    % axis(axH, 'off')
-    % camTar = [0 0 0];
-    % camNorm = [-1 0 0];
-    % set(axH, 'CameraTarget',camTar);
-    % set(axH, 'CameraPosition',camTar + camNorm*700);
-    % set(axH, 'CameraUpVector',normalizeVector3d([0 1 0]));
-    % set(axH, 'CameraViewAngle',10)
-    % set(figH, 'GraphicsSmoothing','off')
-    % export_fig('Figure7', '-tif', '-r300')
 end
 
 end
